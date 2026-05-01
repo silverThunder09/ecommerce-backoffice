ca53a5ee74d962fab41bf9a8fd337561e7c4acf3
silverThunder09 <jeg9017@gmail.com>
2026-05-01 22:36:08 +0900

    feat: webconfig versel 서버 경로 추가3

--- src/main/java/com/ecommercebackoffice/config/WebConfig.java
+++ src/main/java/com/ecommercebackoffice/config/WebConfig.java
@@ -35,10 +35,11 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:5500",
                         "http://127.0.0.1:5500",
                         "http://localhost:63342",  // IntelliJ 내장 서버
-                        "null",
-                        "https://front-backoffice-jeg9017-8364s-projects.vercel.app/", // versel 서버
+                        "https://front-backoffice-jeg9017-8364s-projects.vercel.app/",
+                        "https://front-backoffice-khaki.vercel.app/", // versel 서버
                         ".allowedHeaders(\"*\")", // 모든 헤더 허용
-                        ".allowCredentials(true);" // 쿠키/인증 정보 포함 시 필수"
+                        ".allowCredentials(true);",
+                        "null"// 쿠키/인증 정보 포함 시 필수"
 
                 )
                 // OPTIONS 메서드를 명시적으로 허용
